68cedd290a35ba02877912946fc826af93d2f0a6
M4rc3l <24279888+M4rc3l86@users.noreply.github.com>
2023-12-08 12:36:31 +0100

    combined gui

--- calculator/frontend/src/app/app.component.ts
+++ calculator/frontend/src/app/app.component.ts
@@ -1,13 +1,113 @@
-import { Component } from "@angular/core";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { Component, HostBinding, inject } from "@angular/core";
+import { ButtonComponent } from "./components/button/button.component";
+import { FormsModule } from "@angular/forms";
+import { CalculatorService } from "./services/calculator/calculator.service";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
+  imports: [ButtonComponent, FormsModule],
   selector: "calc-root",
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = "frontend";
+  @HostBinding("class") class = "w-8rem block";
+  protected display = "0";
+
+  protected buttonLabels = [
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ",",
+    "=",
+    "+",
+  ];
+
+  #calculatorService = inject(CalculatorService);
+  #first = 0;
+  #second = 0;
+  #operand = "";
+
+  onButtonClick(input: string): void {
+    if (this.#isDigit(input)) {
+      this.#handleDigit(input);
+    } else if (this.#isOperand(input)) {
+      this.#handleOperand(input);
+    } else if (this.#isEquals(input)) {
+      this.#handleEquals();
+    } else if (this.#isComma(input)) {
+      this.#handleComma();
+    }
+  }
+
+  #isDigit(input: string): boolean {
+    return input >= "0" && input <= "9";
+  }
+
+  #isOperand(input: string): boolean {
+    return input === "+" || input === "-" || input === "*" || input === "/";
+  }
+
+  #isEquals(input: string): boolean {
+    return input === "=";
+  }
+
+  #isComma(input: string): boolean {
+    return input === ",";
+  }
+
+  #handleDigit(input: string): void {
+    if (this.#operand === "") {
+      if (this.display.includes(".")) {
+        this.#first = Number(this.display + input);
+      } else {
+        this.#first = this.#first * 10 + Number(input);
+      }
+      this.display = this.#first.toString();
+    } else {
+      if (this.#second === 0 && this.display.includes(".")) {
+        this.#second = this.#second * 10 + Number(input);
+      } else if (this.display.includes(".")) {
+        this.#second = Number(this.display + input);
+      } else {
+        this.#second = this.#second * 10 + Number(input);
+      }
+      this.display = this.#second.toString();
+    }
+  }
+
+  #handleOperand(input: string): void {
+    this.#operand = input;
+  }
+
+  #handleEquals(): void {
+    let result = 0;
+    this.#calculatorService.calculate(this.#operand, this.#first, this.#second);
+
+    this.display = result.toString();
+    this.#first = result;
+    this.#second = 0;
+    this.#operand = "";
+  }
+
+  #handleComma(): void {
+    if (this.#operand === "") {
+      this.display = this.#first.toString() + ".";
+      console.log(this.#first);
+    } else {
+      this.display = this.#second.toString() + ".";
+
+      console.log(this.#second);
+    }
+  }
 }
